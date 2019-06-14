e161effdf6e7eb2a7af6217bd3356327cd3fc5be
dzimiks <vana997@gmail.com>
2019-06-14 12:04:15 +0200

    Add auth

---
+++ public/js/auth.js
@@ -0,0 +1,109 @@
+$(document).ready(() => {
+	$('#form-signin').submit((e) => {
+		e.preventDefault();
+
+		let email = $('#signinEmail').val().trim();
+		let password = $('#signinPassword').val().trim();
+		let validEmail;
+		let validPassword;
+
+		$(".error").remove();
+
+		if (email.length < 1) {
+			$('#signinEmail').after('<span class="error">This field is required!</span>');
+		} else {
+			const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+			validEmail = regex.test(email);
+
+			if (!validEmail) {
+				$('#signinEmail').after('<span class="error">Enter a valid email!</span>');
+			}
+		}
+
+		if (password.length < 10) {
+			$('#signinPassword').after('<span class="error">Password must be at least 10 characters long!</span>');
+		} else {
+			let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{10,}$/;
+			validPassword = regex.test(password);
+
+			if (!validPassword) {
+				$('#signinPassword').after('<span class="error">You need minimum 10 characters, at least one uppercase letter, one lowercase letter and one number.</span>');
+			}
+		}
+
+		if (validEmail && validPassword) {
+			let url = window.location.href;
+			url = url.substring(0, url.lastIndexOf('/'));
+			url = url.substring(0, url.lastIndexOf('/'));
+			window.location.href = url;
+		}
+	});
+
+	$('#form-signup').submit((e) => {
+		e.preventDefault();
+
+		let firstName = $('#firstNameSignup').val().trim();
+		let lastName = $('#lastNameSignup').val().trim();
+		let username = $('#usernameSignup').val().trim();
+		let email = $('#emailSignup').val().trim();
+		let password = $('#passwordSignup').val().trim();
+
+		let validEmail;
+		let validPassword;
+
+		$(".error").remove();
+
+		if (firstName.length < 1) {
+			$('#firstNameSignup').after('<span class="error">This field is required!</span>');
+		}
+
+		if (lastName.length < 1) {
+			$('#lastNameSignup').after('<span class="error">This field is required!</span>');
+		}
+
+		if (username.length < 3) {
+			$('#usernameSignup').after('<span class="error">Username must be at least 3 characters long!</span>');
+		}
+
+		if (email.length < 1) {
+			$('#emailSignup').after('<span class="error">This field is required!</span>');
+		} else {
+			const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+			validEmail = regex.test(email);
+
+			if (!validEmail) {
+				$('#emailSignup').after('<span class="error">Enter a valid email!</span>');
+			}
+		}
+
+		if (password.length < 10) {
+			$('#passwordSignup').after('<span class="error">Password must be at least 10 characters long!</span>');
+		} else {
+			let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{10,}$/;
+			validPassword = regex.test(password);
+
+			if (!validPassword) {
+				$('#passwordSignup').after('<span class="error">You need minimum 10 characters, at least one uppercase letter, one lowercase letter and one number.</span>');
+			}
+		}
+
+		$.ajax({
+			type: "POST",
+			url: "/add-user",
+			data: $('#form-signup').serialize(),
+			success: (msg) => {
+				console.log(msg);
+			},
+			error: (e) => {
+				console.log("Error:", e);
+			}
+		});
+
+		if (validEmail && validPassword) {
+			let url = window.location.href;
+			url = url.substring(0, url.lastIndexOf('/'));
+			url = url.substring(0, url.lastIndexOf('/'));
+			window.location.href = url;
+		}
+	});
+});
